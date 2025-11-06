9e5eb8d2265811ab2de4720bc700ff4cfb92726a
personalbuse <defaultuser965@gmail.com>
2025-11-06 18:13:33 -0500

    Correcciones para el uso correcto de Backend
    Eliminación de Migraciones en Cache para migrar correctamente la base de datos
    
    Correcciones menores en URLS
    
    Adicion de "Pillow" en requirements.txt para el funcionamiento correcto con la integración del requerimiento de Imagenes

--- apps/profile_app/models.py
+++ apps/profile_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 from apps.profile_app.subapps.profile.models import *
-from .subapps.imageUpload.models import *
+from .subapps.imageupload.models import *
 from apps.auth_app.models import Usuario
 
--- apps/profile_app/subapps/imageUpload/urls.py
+++
@@ -1,7 +0,0 @@
-# apps/profile_app/imageUpload/urls.py
-from django.urls import path
-
-urlpatterns = [
-    # Rutas de imageUpload
-]
-
