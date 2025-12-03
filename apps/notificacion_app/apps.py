c4fa111f232ca1442bbeff0cfa678d939e86b7df
Jeison Alexis Rodríguez Angarita <jeyconalex123@gmail.com>
2025-12-02 20:32:03 -0500

    fix app

--- apps/notificacion_app/apps.py
+++ apps/notificacion_app/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class NotificacionAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'notificacion_app'
+    name = 'apps.notificacion_app'
 
     def ready(self):
-        import notificacion_app.signals
+        import apps.notificacion_app.signals
